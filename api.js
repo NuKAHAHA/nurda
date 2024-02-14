2c747c19a37e05abbb231877131e03413ca71072
NuKAHAHA <146062616+NuKAHAHA@users.noreply.github.com>
2024-02-14 21:47:12 +0600

    Update api.js

--- api.js
+++ api.js
@@ -1,45 +1,8 @@
 const axios = require('axios');
-const OPENWEATHER_KEY = "79429e941903ab8242cdd151cf7e1994";
 const YOUR_RAPIDAPI_KEY = "68915ed042msh2b157ff89f4020dp14b020jsn76fe01fb6da0";
 const FTBLNEW_KEY = "9ca3a28ed1msh04c032d4660f787p14d241jsne5e26423507f";
 
 
-async function getWeatherByCity(city) {
-    let response, responseData = null;
-    try {
-        response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=en&appid=${OPENWEATHER_KEY}`);
-        responseData = response?.data;
-    } catch {
-        return null;
-    }
-
-    if (!responseData) {
-        return null;
-    }
-    
-    if (responseData.cod !== 200) {
-        return null;
-    }
-
-    return {
-        "latitude": responseData.coord.lat,
-        "longitude": responseData.coord.lon,
-        "description": responseData.weather[0].description,
-        "temperature": Math.floor(responseData.main.temp),
-        "feels_like": responseData.main.feels_like,
-        "pressure": responseData.main.pressure,
-        "maximum_temperature": Math.floor(responseData.main.temp_max),
-        "minimum_temperature": Math.floor(responseData.main.temp_min),
-        "humidity": responseData.main.humidity,
-        "wind_speed": responseData.wind.speed,
-        "wind_deg": responseData.wind.deg,
-        "cloudiness": responseData.clouds.all,
-        "sunrise": new Date(responseData.sys.sunrise * 1000).toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit' }),
-        "sunset": new Date(responseData.sys.sunset * 1000).toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit' }),
-        "name"  : responseData.name,
-        "country" : responseData.sys.country
-    };
-}
 
 async function getLaLigaNews() {
 
